96b543c420939620ec1d1297db8ae77be8c3efef
tsamlama <99736828+tsamlama@users.noreply.github.com>
2022-03-20 16:37:52 +0545

    Add files via upload

---
+++ src/test/java/com/example/AppTest.java
@@ -0,0 +1,21 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Unit test for simple App.
+ */
+class AppTest {
+    /**
+     * Rigorous Test.
+     */
+    @Test
+    void testApp() {
+        assertEquals(1, 1);
+    }
+
+    private void assertEquals(int i, int j) {
+    }
+}
